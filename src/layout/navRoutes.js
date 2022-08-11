4903f165d62f70c7f0d87a0e73fda6059a302d8d
Arkadiusz Machalica <a.machalica@selleo.com>
2022-08-11 09:37:41 +0200

    Fix routing

--- src/layout/navRoutes.js
+++ src/layout/navRoutes.js
@@ -10,6 +10,6 @@ export const navRoutes = [
   { name: 'framer-motion', to: '/framer-motion', Component: FramerMotionPage },
   { name: 'react-motion', to: '/react-motion', Component: ReactMotionPage },
   { name: 'react-spring', to: '/react-spring', Component: ReactSpringPage },
-  { name: 'gsap', to: '/react-spring', Component: GSAPPage },
+  { name: 'gsap', to: '/gsap', Component: GSAPPage },
   { name: 'reanimated', to: '/reanimated', Component: ReanimatedPage },
 ]
