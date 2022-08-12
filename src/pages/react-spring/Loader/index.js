5b3142df5179463b5015bb60eb37a10bceb89125
Dawid Kawiak <d.kawiak@selleo.com>
2022-08-12 12:15:34 +0200

    Add move, add flag

--- src/pages/react-spring/Loader/index.js
+++ src/pages/react-spring/Loader/index.js
@@ -1,8 +1,11 @@
+import { useState } from 'react'
 import { animated, useSpring } from 'react-spring'
 
 import car from './car.png'
+import flag from './flag.png'
 
 const Loader = () => {
+  const [move, setMove] = useState({})
   const styles = useSpring({
     loop: true,
     to: [
@@ -14,9 +17,71 @@ const Loader = () => {
     from: { transform: 'skew(0, 0)' },
   })
 
+  const stylesRight = useSpring({
+    loop: true,
+    to: [
+      { transform: 'skew(0, 0)' },
+      { transform: 'skew(0, 15deg)' },
+      { transform: 'skew(0, -15deg)' },
+      { transform: 'skew(0, 15deg)' },
+      { position: 'absolute' },
+      { left: 300 },
+    ],
+    from: { transform: 'skew(0, 0)' },
+  })
+
+  const stylesDown = useSpring({
+    loop: true,
+    to: [
+      { transform: 'skew(0, 0)' },
+      { transform: 'skew(0, 15deg)' },
+      { transform: 'skew(0, -15deg)' },
+      { transform: 'skew(0, 15deg)' },
+      { position: 'absolute' },
+      { left: 300 },
+      { top: 300 },
+    ],
+    from: { transform: 'skew(0, 0)' },
+  })
+
+  const renderStyles = () => {
+    console.log('move', move)
+    if (move === 'right') return stylesRight
+    else if (move === 'down') return stylesDown
+    else return styles
+  }
+
   return (
     <>
-      <animated.img style={styles} src={car} alt="car" />
+      <div
+        style={{
+          position: 'fixed',
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0,
+          backgroundColor: 'white',
+        }}
+      >
+        <animated.img style={renderStyles()} src={car} alt="car" />
+        <img
+          style={{
+            width: 100,
+            width: 100,
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+          }}
+          src={flag}
+          alt="flag"
+        />
+        <div style={{ position: 'fixed', right: 20, bottom: 20 }}>
+          <button style={{ marginRight: 60 }} onClick={() => setMove('down')}>
+            DOWN
+          </button>
+          <button onClick={() => setMove('right')}>RIGHT</button>
+        </div>
+      </div>
     </>
   )
 }
