4d66e5471650fd08c076174fd7fcedd326046d9e
Dawid Kawiak <d.kawiak@selleo.com>
2022-09-16 11:41:55 +0200

    Move car in loop

--- src/pages/react-spring/Loader/index.js
+++ src/pages/react-spring/Loader/index.js
@@ -5,53 +5,74 @@ import car from './car.png'
 import flag from './flag.png'
 
 const Loader = () => {
-  const [move, setMove] = useState({})
   const [isLoading, setIsLoading] = useState(true)
   const styles = useSpring({
     loop: true,
-    to: [
-      { transform: 'skew(0, 0)' },
-      { transform: 'skew(0, 15deg)' },
-      { transform: 'skew(0, -15deg)' },
-      { transform: 'skew(0, 15deg)' },
-    ],
-    from: { transform: 'skew(0, 0)' },
+    to: [{ left: '60%' }],
+    from: {
+      position: 'absolute',
+      left: '0%',
+      top: '35%',
+      zIndex: 9999,
+    },
+    config: { mass: 3, tension: 40, friction: 20, clamp: true },
   })
 
-  const stylesRight = useSpring({
+  const stylesFlag1 = useSpring({
     loop: true,
-    to: [
-      { transform: 'skew(0, 0)' },
-      { transform: 'skew(0, 15deg)' },
-      { transform: 'skew(0, -15deg)' },
-      { transform: 'skew(0, 15deg)' },
-      { position: 'absolute' },
-      { left: 300 },
-    ],
-    from: { transform: 'skew(0, 0)' },
+    to: [{ top: '52%' }, { top: '48%' }, { top: '50%' }],
+    from: {
+      position: 'fixed',
+      top: '50%',
+      left: '50%',
+      width: 100,
+      height: 100,
+    },
+    config: { mass: 3, tension: 300, friction: 20, clamp: true },
   })
 
-  const stylesDown = useSpring({
+  const stylesFlag2 = useSpring({
     loop: true,
-    to: [
-      { transform: 'skew(0, 0)' },
-      { transform: 'skew(0, 15deg)' },
-      { transform: 'skew(0, -15deg)' },
-      { transform: 'skew(0, 15deg)' },
-      { position: 'absolute' },
-      { left: 300 },
-      { top: 300 },
-    ],
-    from: { transform: 'skew(0, 0)' },
+    to: [{ top: '22%' }, { top: '18%' }, { top: '20%' }],
+    from: {
+      width: 100,
+      height: 100,
+      position: 'fixed',
+      top: '20%',
+      left: '50%',
+    },
+    config: { mass: 3, tension: 300, friction: 20, clamp: true },
   })
 
-  const renderStyles = () => {
-    console.log('move', move)
-    if (move === 'right') return stylesRight
-    else if (move === 'down') return stylesDown
-    else return styles
-  }
+  const stylesTyre1 = useSpring({
+    loop: true,
+    to: [{ right: '38%', left: '15%' }],
+    from: {
+      height: 12,
+      backgroundColor: '#000000',
+      position: 'fixed',
+      left: '0%',
+      right: '100%',
+      top: '37%',
+      opacity: 0.7,
+    },
+    config: { mass: 3, tension: 40, friction: 20, clamp: true },
+  })
 
+  const stylesTyre2 = useSpring({
+    loop: true,
+    to: [{ right: '38%', left: '15%' }],
+    from: {
+      height: 12,
+      backgroundColor: '#000000',
+      position: 'fixed',
+      left: '0%',
+      right: '100%',
+      top: '42.5%',
+      opacity: 0.7,
+    },
+    config: { mass: 3, tension: 40, friction: 20, clamp: true },
+  })
   return (
     isLoading && (
       <>
@@ -65,24 +86,11 @@ const Loader = () => {
             backgroundColor: 'white',
           }}
         >
-          <animated.img style={renderStyles()} src={car} alt="car" />
-          <img
-            style={{
-              width: 100,
-              height: 100,
-              position: 'fixed',
-              top: '50%',
-              left: '50%',
-            }}
-            src={flag}
-            alt="flag"
-          />
-          <div style={{ position: 'fixed', right: 20, bottom: 20 }}>
-            <button style={{ marginRight: 60 }} onClick={() => setMove('down')}>
-              DOWN
-            </button>
-            <button onClick={() => setMove('right')}>RIGHT</button>
-          </div>
+          <animated.img style={styles} src={car} alt="car" />
+          <animated.img style={stylesFlag1} src={flag} alt="flag" />
+          <animated.img style={stylesFlag2} src={flag} alt="flag" />
+          <animated.div style={stylesTyre1} />
+          <animated.div style={stylesTyre2} />
         </div>
       </>
     )
