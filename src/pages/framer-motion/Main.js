b62a5cb0e5fd8118e7929919385f542af3e63980
snacksncode <snacksncode@gmail.com>
2022-09-16 11:51:50 +0200

    Adjusted entry animation

--- src/pages/framer-motion/Main.js
+++ src/pages/framer-motion/Main.js
@@ -9,6 +9,7 @@ import { List } from './components/List'
 
 export const Main = () => {
   const [isLoading, setIsLoading] = useState(true)
+  const [showContent, setShowContent] = useState(false)
 
   useEffect(() => {
     const loadingTimeout = setTimeout(() => {
@@ -24,28 +25,36 @@ export const Main = () => {
         {'Framer motion'.split('').map((letter, index) => {
           if (letter === ' ')
             return (
-              <div style={{ width: '0.75ch', display: 'inline-block' }}></div>
+              <motion.div
+                style={{ display: 'inline-block' }}
+                initial={{ width: '5ch' }}
+                animate={{ width: '2ch' }}
+              ></motion.div>
             )
 
           return (
             <motion.span
-              className="text-6xl font-bold text-purple-400"
+              className="font-bold m-0 leading-none text-purple-400"
               style={{ position: 'relative', display: 'inline-block' }}
               initial={{
-                y: 20,
-                x: 10 * index,
-                rotate: 30 * index,
-                scale: index / 2,
-                opacity: 0,
+                top: '50vh',
+                y: 'calc(-50% - 88px)',
+                fontSize: '10rem',
+                opacity: 0.5,
+              }}
+              onAnimationComplete={() => {
+                if (index !== 12) return
+                setShowContent(true)
               }}
               animate={{
                 y: 0,
-                scale: 1,
-                opacity: 1,
-                x: 0,
-                rotate: 0,
-                color: `rgb(200 ${15 * index} ${30 * index})`,
+                top: 0,
                 transition: { delay: 0.05 * index },
+                fontSize: '3.75rem',
+                opacity: 1,
+                //   x: 0,
+                rotate: 0,
+                color: `hsl(${index * 5 + 250} 80 45)`,
               }}
             >
               {letter}
@@ -53,8 +62,12 @@ export const Main = () => {
           )
         })}
       </div>
-      <Form />
-      <List />
+      {showContent && (
+        <>
+          <Form />
+          <List />
+        </>
+      )}
     </>
   )
 }
