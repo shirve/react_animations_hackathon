df8d458f8676328554bdada73dfb1518fa63a2c0
snacksncode <snacksncode@gmail.com>
2022-09-16 11:34:01 +0200

    This shit is awesome

--- src/pages/framer-motion/Main.js
+++ src/pages/framer-motion/Main.js
@@ -20,13 +20,39 @@ export const Main = () => {
   if (isLoading) return <ListLoader number={20} />
   return (
     <>
-      <motion.div
-        className="text-xl	uppercase tracking-wide text-purple-400 text-center"
-        initial={{ x: '-100%' }}
-        animate={{ x: 0 }}
-      >
-        Framer motion
-      </motion.div>
+      <div className="flex justify-center">
+        {'Framer motion'.split('').map((letter, index) => {
+          if (letter === ' ')
+            return (
+              <div style={{ width: '0.75ch', display: 'inline-block' }}></div>
+            )
+
+          return (
+            <motion.span
+              className="text-6xl font-bold text-purple-400"
+              style={{ position: 'relative', display: 'inline-block' }}
+              initial={{
+                y: 20,
+                x: 10 * index,
+                rotate: 30 * index,
+                scale: index / 2,
+                opacity: 0,
+              }}
+              animate={{
+                y: 0,
+                scale: 1,
+                opacity: 1,
+                x: 0,
+                rotate: 0,
+                color: `rgb(200 ${15 * index} ${30 * index})`,
+                transition: { delay: 0.05 * index },
+              }}
+            >
+              {letter}
+            </motion.span>
+          )
+        })}
+      </div>
       <Form />
       <List />
     </>
